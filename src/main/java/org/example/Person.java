f5fbe55b8ca84b574cbc63b238e8d27262bb2012
yahya <yhia90702@gmail.com>
2026-04-27 14:39:46 +0300

    None

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Person {
+    private String name;
+    private int id;
+    private String password;
+    public Person(String name,int id, String password){
+        this.password = password;
+        this.name = name;
+        this.id = id;
+    }
+    // Gets methods
+    public String getName(){return name;}
+    public int getId(){return id;}
+    public String getPassword(){return password;}
+    // Sets methods
+    public void setName(String name){this.name = name;}
+    public void setId(int id){this.id = id;}
+    public void getPassword(String PhoneNumber){this.password = password;}
+}
