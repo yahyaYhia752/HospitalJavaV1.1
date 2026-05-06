84c14a70b2ba6cf9657df7605be3d371094e72af
TALAL-51 <tllzxd453@gmail.com>
2026-05-06 18:48:13 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,6 @@ package org.example;
 
 public class Main {
     static void main() {
-        Patient pat1 = new Patient(
-                "ahmed",
-                1,
-                "1234567",
-                1,
-                statusPaitent.STABLE,
-                "Headache"
-        );
-        IO.println(pat1.getPassword());
-        IO.println(pat1.toString());
-        pat1.setPassword("100");
-        IO.println(pat1.toString());
+
     }
 }
