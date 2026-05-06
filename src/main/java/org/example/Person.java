2261e04846ff560518b8c9db74463e0268170c99
TALAL-51 <tllzxd453@gmail.com>
2026-05-06 18:49:31 +0300

    Update Person.java

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -5,33 +5,42 @@ public class Person {
     private String name;
     private int id;
     private String password;
+    private static int count;
+
     public Person(String name,int id, String password){
+        count += 1;
         this.password = password;
         this.name = name;
         this.id = id;
     }
-//getters
+    //getters
     public String getName() {
         return name;
     }
-
     public int getId() {
         return id;
     }
-
     public String getPassword() {
         return password;
     }
+
     //setters
     public void setName(String name) {
         this.name = name;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public void setPassword(String password) {
         this.password = password;
+    }
+    public static int getCount() {return count;}
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
